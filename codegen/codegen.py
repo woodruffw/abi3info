2254bc880c8d3552bb72b30d9dfb5b266e50a586
William Woodruff <william@yossarian.net>
2022-09-19 13:53:00 -0400

    codegen: logging, check mtime on codegen itself
    Signed-off-by: William Woodruff <william@yossarian.net>

--- codegen/codegen.py
+++ codegen/codegen.py
@@ -27,7 +27,8 @@ from abi3info.models import (
 # Sanity checks.
 assert shutil.which("black"), "codegen needs `black` for auto-formatting!"
 
-_HERE = Path(__file__).resolve().parent
+_THIS = Path(__file__).resolve()
+_HERE = _THIS.parent
 
 _ABI3INFO = _HERE.parent / "abi3info"
 assert _ABI3INFO.is_dir(), "missing abi3info module"
@@ -39,17 +40,31 @@ stable_abi_data = _STABLE_ABI_FILE.read_text()
 new_checksum = sha256(stable_abi_data.encode()).hexdigest()
 
 _STABLE_ABI_CHECKSUM_FILE = _STABLE_ABI_FILE.with_suffix(".sha256")
-old_checksum = None
+stable_abi_changed = False
 if _STABLE_ABI_CHECKSUM_FILE.is_file():
     old_checksum = _STABLE_ABI_CHECKSUM_FILE.read_text().rstrip()
+    stable_abi_changed = new_checksum != old_checksum
 
-if old_checksum == new_checksum and not os.getenv("FORCE_CODEGEN", False):
-    print("codegen: exiting early because input has not changed")
+_INTERNAL = _ABI3INFO / "_internal.py"
+internal_outdated = False
+if _INTERNAL.is_file():
+    internal_mtime = _INTERNAL.stat().st_mtime
+    codegen_mtime = _THIS.stat().st_mtime
+    internal_outdated = codegen_mtime >= internal_mtime
+
+force_codegen = bool(os.getenv("FORCE_CODEGEN", False))
+
+print(f"[+] {stable_abi_changed=} {internal_outdated=} {force_codegen=}")
+
+# We skip codegen if all of the following are true:
+# * `stable_abi.toml` has not changed since the last run
+# * `_internal.py` is newer than this file (meaning we haven't tweaked it)
+# * `FORCE_CODEGEN` is not set
+if not stable_abi_changed and not internal_outdated and not force_codegen:
+    print("[!] codegen: exiting early because nothing has changed", file=sys.stderr)
     sys.exit(0)
 
 _STABLE_ABI_CHECKSUM_FILE.write_text(new_checksum)
-
-# TODO: Check here to see if _STABLE_ABI_FILE has changed or if we're forcing a run anyways.
 
 _STABLE_ABI_DATA = toml.loads(stable_abi_data)
 for key in ("struct", "function", "data"):
@@ -57,7 +72,6 @@ for key in ("struct", "function", "data"):
     val = _STABLE_ABI_DATA[key]
     assert isinstance(val, dict), "stable ABI data doesn't look right (format changed?)"
 
-_INTERNAL = _ABI3INFO / "_internal.py"
 _OUT = _INTERNAL.open(mode="w")
 
 # All generated code goes into `abi3info/_internal.py`.
@@ -86,12 +100,14 @@ print(
 
 print("# this file was generated; do not modify it by hand!", file=_OUT)
 
+print("[+] codgen: feature macros", file=sys.stderr)
 feature_macros = {
     name: FeatureMacro(name, body["doc"], body.get("windows", False))
     for name, body in _STABLE_ABI_DATA["feature_macro"].items()
 }
 print(f"_FEATURE_MACROS: Final[dict[str, FeatureMacro]] = {feature_macros}", file=_OUT)
 
+print("[+] codgen: structs", file=sys.stderr)
 structs = {}
 for name, body in _STABLE_ABI_DATA["struct"].items():
     match body["struct_abi_kind"]:
@@ -107,6 +123,7 @@ for name, body in _STABLE_ABI_DATA["struct"].items():
     structs[struct.name] = struct
 print(f"_STRUCTS: Final[dict[str, Struct]] = {structs}", file=_OUT)
 
+print("[+] codgen: functions", file=sys.stderr)
 functions = {
     Symbol(name): Function(
         Symbol(name),
@@ -118,11 +135,13 @@ functions = {
 }
 print(f"_FUNCTIONS: Final[dict[Symbol, Function]] = {functions}", file=_OUT)
 
+print("[+] codgen: macros", file=sys.stderr)
 macros = {}
 for name, body in {**_STABLE_ABI_DATA["const"], **_STABLE_ABI_DATA["macro"]}.items():
     macros[name] = Macro(name, PyVersion.parse(body["added"]))
 print(f"_MACROS: Final[dict[str, Macro]] = {macros}", file=_OUT)
 
+print("[+] codgen: data objects", file=sys.stderr)
 datas = {
     Symbol(name): Data(
         Symbol(name),
@@ -134,6 +153,7 @@ datas = {
 }
 print(f"_DATAS: Final[dict[Symbol, Data]] = {datas}", file=_OUT)
 
+print("[+] codgen: typedefs", file=sys.stderr)
 typedefs = {
     name: Typedef(name, PyVersion.parse(body["added"]))
     for name, body in _STABLE_ABI_DATA["typedef"].items()
@@ -142,4 +162,7 @@ print(f"_TYPEDEFS: Final[dict[str, Typedef]] = {typedefs}", file=_OUT)
 
 _OUT.close()
 
-subprocess.run(["black", _INTERNAL])
+print("[+] codgen: reformatting", file=sys.stderr)
+subprocess.run(["black", _INTERNAL], check=True)
+
+print("[+] codegen: all done!", file=sys.stderr)
