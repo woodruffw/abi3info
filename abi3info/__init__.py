f52c4b690582055b543889dbb13782ff66842767
William Woodruff <william@yossarian.net>
2022-09-19 11:40:13 -0400

    abi3info: expose internal APIs in a controlled manner
    Signed-off-by: William Woodruff <william@yossarian.net>

--- abi3info/__init__.py
+++ abi3info/__init__.py
@@ -2,7 +2,25 @@
 The abi3info APIs.
 """
 
+from abi3info._internal import (
+    DATAS,
+    FEATURE_MACROS,
+    FUNCTIONS,
+    MACROS,
+    STRUCTS,
+    TYPEDEFS,
+)
+
 __version__ = "0.0.1"
 """
 The current version of abi3info.
 """
+
+__all__ = [
+    "FEATURE_MACROS",
+    "STRUCTS",
+    "FUNCTIONS",
+    "MACROS",
+    "DATAS",
+    "TYPEDEFS",
+]
