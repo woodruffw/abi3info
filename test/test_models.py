7efa38e615ba3e4b4d016d2920ed118e3af76f7e
William Woodruff <william@yossarian.net>
2022-09-26 20:47:47 -0400

    test: add remaining homoiconicity tests

--- test/test_models.py
+++ test/test_models.py
@@ -1,6 +1,19 @@
+import itertools
+
 import pytest
 
-from abi3info.models import PyVersion, Symbol
+from abi3info.models import (
+    Data,
+    FeatureMacro,
+    FullStruct,
+    Function,
+    Macro,
+    OpaqueStruct,
+    PartialStruct,
+    PyVersion,
+    Symbol,
+    Typedef,
+)
 
 
 class TestSymbol:
@@ -26,6 +39,9 @@ class TestSymbol:
 
 
 class TestPyVersion:
+    def test_homoiconic(self):
+        assert eval(repr(PyVersion(3, 10))) == PyVersion(3, 10)
+
     def test_comparison(self):
         older_minor, newer_minor = PyVersion(3, 2), PyVersion(3, 3)
 
@@ -98,5 +114,62 @@ class TestPyVersion:
         assert str(PyVersion(3, 2)) == "3.2"
         assert f"{PyVersion(3, 2)}" == "3.2"
 
+
+class TestMacro:
     def test_homoiconic(self):
-        assert eval(repr(PyVersion(3, 10))) == PyVersion(3, 10)
+        assert eval(repr(Macro("foo", PyVersion(3, 10)))) == Macro("foo", PyVersion(3, 10))
+
+
+class TestOpaqueStruct:
+    def test_homoiconic(self):
+        assert eval(repr(OpaqueStruct("foo", PyVersion(3, 10)))) == OpaqueStruct(
+            "foo", PyVersion(3, 10)
+        )
+
+
+class TestPartialStruct:
+    def test_homoiconic(self):
+        for members in (None, ["bar"]):
+            assert eval(repr(PartialStruct("foo", PyVersion(3, 10), members))) == PartialStruct(
+                "foo", PyVersion(3, 10), members
+            )
+
+
+class TestFullStruct:
+    def test_homoiconic(self):
+        assert eval(repr(FullStruct("foo", PyVersion(3, 10)))) == FullStruct(
+            "foo", PyVersion(3, 10)
+        )
+
+
+class TestFeatureMacro:
+    def test_homoiconic(self):
+        for windows in (True, False, "maybe"):
+            assert eval(repr(FeatureMacro("foo", "some doc", windows))) == FeatureMacro(
+                "foo", "some doc", windows
+            )
+
+
+class TestFunction:
+    def test_homoiconic(self):
+        for ifdef, abi_only in itertools.product(
+            (None, FeatureMacro("foo", "some doc", True)), (True, False)
+        ):
+            assert eval(
+                repr(Function(Symbol("foo"), PyVersion(3, 10), ifdef, abi_only))
+            ) == Function(Symbol("foo"), PyVersion(3, 10), ifdef, abi_only)
+
+
+class TestData:
+    def test_homoiconic(self):
+        for ifdef, abi_only in itertools.product(
+            (None, FeatureMacro("foo", "some doc", True)), (True, False)
+        ):
+            assert eval(repr(Data(Symbol("foo"), PyVersion(3, 10), ifdef, abi_only))) == Data(
+                Symbol("foo"), PyVersion(3, 10), ifdef, abi_only
+            )
+
+
+class TestTypedef:
+    def test_homoiconic(self):
+        assert eval(repr(Typedef("foo", PyVersion(3, 10)))) == Typedef("foo", PyVersion(3, 10))
