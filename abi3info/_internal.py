0e5f3b474cdd11cca011ade97dee2cbd24a9ac51
woodruffw <3059210+woodruffw@users.noreply.github.com>
2024-10-26 12:07:57 +0000

    [BOT] update stable_abi.toml

--- abi3info/_internal.py
+++ abi3info/_internal.py
@@ -4737,6 +4737,12 @@ _FUNCTIONS: Final[dict[Symbol, Function]] = {
         ifdef=None,
         abi_only=False,
     ),
+    Symbol(name="PyType_Freeze", visibility=None): Function(
+        symbol=Symbol(name="PyType_Freeze", visibility=None),
+        added=PyVersion(major=3, minor=14),
+        ifdef=None,
+        abi_only=False,
+    ),
 }
 _MACROS: Final[dict[str, Macro]] = {
     "Py_tp_dealloc": Macro(name="Py_tp_dealloc", added=PyVersion(major=3, minor=2)),
